d8c71660c065c4a750cdfdad9391b942bba1cc0d
drose98 <dylan.a.rose@gmail.com>
2017-10-03 02:11:28 -0400

    10-2-17 night, desktop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("this test works");
+
+            JFrame myFrame = new JFrame("Sample Frame");
+            myFrame.setSize(800,450);
+            myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            myFrame.setVisible(true);
     }
 }
